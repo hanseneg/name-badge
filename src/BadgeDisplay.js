5b84d6f691d54d9cc0f0647ff184da68caa88cb6
Ethan Hansen <hanseneg@hotmail.com>
2020-11-12 14:03:45 -0700

    commit3

--- src/BadgeDisplay.js
+++ src/BadgeDisplay.js
@@ -5,14 +5,14 @@ class BadgeDisplay extends React.Component{
     render() {
         const mappedBadges = this.props.info.nameBadges.map(badge => {
             return (
-                <div className="badgeContainer">
-                <h1 className='h1'>Badge:</h1>
-                    <div className="badgeLeft1"> Name: {badge.firstName + ' ' + badge.lastName}</div>
-                    <div className="badgeLeft2"> Place of Birth: {badge.placeOfBirth}</div>
-                    <div className="badgeLeft3"> Email: {badge.email}</div>
-                    <div className="badgeRight1"> Phone: {'(' + badge.phone.slice(0, 3) + ') ' + badge.phone.slice(3, 6) + '-' + badge.phone.slice(6)}</div>
-                    <div className="badgeRight2"> Favorite food: {badge.favFood}</div>
-                    <div className="aboutYouDisplay">{badge.aboutYou}</div>
+                <div className="badge">
+                    <h1 className="badgeTag">Badge:</h1>
+                    <div className="name"> Name: {badge.firstName + ' ' + badge.lastName}</div>
+                    <div className="placeOfBirth"> Place of Birth: {badge.placeOfBirth}</div>
+                    <div className="email"> Email: {badge.email}</div>
+                    <div className="phone"> Phone: {'(' + badge.phone.slice(0, 3) + ') ' + badge.phone.slice(3, 6) + '-' + badge.phone.slice(6)}</div>
+                    <div className="favFood"> Favorite food: {badge.favFood}</div>
+                    <div className="aboutYou">{badge.aboutYou}</div>
                 </div>
             )
         })  
